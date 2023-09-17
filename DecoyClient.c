2f76d9af675b29143556335d07b68e21bb9dd32c
unknown <adityabanka1947@gmail.com>
2023-09-18 03:11:32 +0530

    DecoyChanges

--- DecoyClient.c
+++ DecoyClient.c
@@ -9,79 +9,72 @@ the code such as, state of printing, baud rate, and other related capabilities.
 */
 struct CLIENT_STATE
 {
-    int printing_state; //0 - disable, 1 - enable
-}clientState;
+    int printing_state; // 0 - disable, 1 - enable
+} clientState;
 
 void initialize_WSA_API()
 {
+    printf("Initializing WSA startup..\n");
+    printf("1. WSA startup :\t");
     WSADATA wsaData;
     int returnValue = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if(returnValue != 0)
+    if (returnValue != 0)
     {
-        printf("%d : WSA startup : failed.\n", returnValue);
+        printf("Failed due->%d\n", returnValue);
         return;
     }
-    else
-    {
-        printf("WSA successfully initialized.\n");
-    }
+    printf("Successful\n");
+    printf("\n");
     return;
 }
 
-void createClient(char *IP_Address, int portNumber)
+SOCKET createSocket(char *IP_Address, int portNumber)
 {
+    // creating the socket
     printf("Initializing socket..\n");
+    printf("1. Socket Creation :\t");
+    SOCKET socketFD = INVALID_SOCKET;
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD == INVALID_SOCKET)
+    {
+        printf("Failed due->%d\n", WSAGetLastError());
+        WSACleanup();
+        return (0);
+    }
+    printf("Successful\n");
+
+    // connecting to the server
+    printf("2. Connecting :\t");
     struct sockaddr_in address;
     address.sin_addr.s_addr = inet_addr(IP_Address);
     address.sin_family = AF_INET;
     address.sin_port = hton(portNumber);
-
-    printf("1. Socket Creation : ");
-    SOCKET socketFD = INVALID_SOCKET;
-    socketFD = socket(AF_INET, SOCK_STREAM, 0);
-    if(socketFD == INVALID_SOCKET)
+    int returnValue = connect(socketFD, (struct sockaddr *)&address, sizeof(address));
+    if (returnValue == SOCKET_ERROR)
     {
         printf("Failed due->%d\n", WSAGetLastError());
         WSACleanup();
-        return;
+        return (0);
     }
+    printf("Successful\n");
+    return (socketFD);
 }
 
 int main()
 {
     clientState.printing_state = 1;
 
-    SOCKET sock = INVALID_SOCKET;
-    struct sockaddr_in serverAddr;
-
-    // Create a socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET)
+    //getting the socket
+    printf("Creating socket..\n");
+    SOCKET socketFD = createSocket("127.0.0.1", 8000);
+    printf("3. Receiving :\t");
+    if(socketFD == 0)
     {
-        printf("Socket creation failed: %d\n", WSAGetLastError());
-        WSACleanup();
-        return 1;
+        printf("Failed\n");
     }
-
-    // Set up the server address structure
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(8000);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    // Connect to the server
-    int ret = connect(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
-    if (ret == SOCKET_ERROR)
-    {
-        std::cerr << "Connect failed: " << WSAGetLastError() << "\n";
-        closesocket(sock);
-        WSACleanup();
-        return 1;
-    }
-
-    std::cout << "Connected to the server!\n";
-
-    // Clean up and close the socket
-    closesocket(sock);
+    printf("Successful\n");
+    printf("Socket is successfully created, connected and received\n");
+    closesocket(socketFD);
     WSACleanup();
 
     return 0;
